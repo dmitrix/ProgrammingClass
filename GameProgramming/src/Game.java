1ba484bd75b5a0c6e4ded14b6f8a5a8bd1540abd
dmitrix <marcelbaarsch@gmail.com>
2015-11-05 17:09:28 -0800

    Episode 4 - Window

--- GameProgramming/src/Game.java
+++ GameProgramming/src/Game.java
@@ -1,7 +1,10 @@
 import java.awt.Canvas;
 import javax.swing.JFrame;
+import java.awt.Dimension;
 
 public class Game extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
@@ -13,7 +16,10 @@ public class Game extends Canvas implements Runnable {
 	
 	// constructor
 	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
 		
+		frame = new JFrame();
 	}
 	
 	public synchronized void start() {
@@ -37,5 +43,20 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 	
+	public static void main(String[] args){
+		Game game = new Game();
+		game.frame.setResizable(false); // make sure this is the first thing to frame
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null); // center window
+		game.frame.setVisible(true); // so we can actually see it
+		
+		game.start();
+		
+		
+		
+	}
 	
 }
