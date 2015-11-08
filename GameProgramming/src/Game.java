e3cad32a7480d3e2b7f2b861aff7200326f3a0a3
dmitrix <marcelbaarsch@gmail.com>
2015-11-08 09:40:46 -0800

    Episode 9 - Rendering Pixels (bugs??)

--- GameProgramming/src/Game.java
+++ GameProgramming/src/Game.java
@@ -70,13 +70,19 @@ public class Game extends Canvas implements Runnable {
 			return; 
 		}
 		
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
 		Graphics g = bs.getDrawGraphics();
 		
 		// All graphics go here
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		//g.setColor(Color.BLACK);
+		//g.fillRect(0, 0, getWidth(), getHeight());
 		
-		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		
 		// End graphics
 		
