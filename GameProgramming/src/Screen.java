c3f1a97a555f6270c04c4654d3add91e08a1cf7e
dmitrix <marcelbaarsch@gmail.com>
2015-11-09 04:00:13 -0800

    Episode 10 - Clearing the Screen

--- GameProgramming/src/Screen.java
+++ GameProgramming/src/Screen.java
@@ -10,6 +10,12 @@ public class Screen{
 		
 	}
 	
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
 	public void render(){
 		for (int y = 0; y < height; y++){
 			for (int x = 0; x < width; x++){
