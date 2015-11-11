b9871c8dacc7b847c2f65f3aa80294f919c72e3a
dmitrix <marcelbaarsch@gmail.com>
2015-11-11 08:59:57 -0800

    template wip

---
+++ Template.java
@@ -0,0 +1,27 @@
+/*
+	Template.java
+
+	Marcel Baarsch
+	marcelbaarsch@gmail.com
+	
+	A code file just to remind me
+	how java syntax works.
+
+*/
+
+import java.util.Scanner.
+
+public class Template {
+	
+	public static void main (String args[])
+	{
+		Scanner kbd = new Scanner(System.in);
+		System.out.printf("This is how to print to console. \n Enter a number please: ");
+		int userNum = kbd.nextInt();
+		System.out.printf("This is 
+		System.out.println("
+	}
+	
+}
+
+
