53f934d4464716097db876a221e29d5c84001742
dmitrix <marcelbaarsch@gmail.com>
2015-11-29 21:40:57 -0800

    Still not done. .___.

--- labs/10/ScoreReader.java
+++ labs/10/ScoreReader.java
@@ -13,9 +13,8 @@
 
     The file format is a text file that has the Title for what these scores represent (like Exam 1) followed by a list of scores where there is 1 score per line. Your code should catch all files that do not have a Title line in the file and still process the scores and include a notation in the output that no Title was found in the file (You only have to worry about the title in the first row of the file).
 */
-// no idea how to file i/o D: -- I think I am supposed to use Scanner somehow????? AHHHHH
 
-// Problem solving rules: Don't get frustrated. Always have a plan.
+// First I Am going to run someone else's example.
 
 import java.io.*;
 import java.util.Scanner; // This is a start.
@@ -23,8 +22,24 @@ import java.util.Scanner; // This is a start.
 public class ScoreReader
 {
   public static void main(String args[]) throws IOException{
-   Scanner s = null;
-
+	try {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the file name with extention : ");
+		File file = new File(input.nextLine());
+	
+		input = new Scanner(file);
+	
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			System.out.println(line);
+		}
+		input.close();
+	}
+	catch (Exception ex){
+		ex.printStackTrace();
+	}
+	
+	
   }
 
 
